b7a8ebd275a0dd568b056420683e733487f98a20
Julien Vitry <juvitry@f0r10s3.clusters.42paris.fr>
2025-06-18 09:53:41 +0200

    includes updated

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: juvitry <juvitry@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 09:07:19 by juvitry           #+#    #+#             */
-/*   Updated: 2025/06/18 09:08:52 by juvitry          ###   ########.fr       */
+/*   Updated: 2025/06/18 09:52:14 by juvitry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,20 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <fcntl.h>
+# include "../libft/libft.h"
+
+# define KEY_W				119
+# define KEY_A				97
+# define KEY_S				115
+# define KEY_D				100
+
+# define KEY_UP				65362
+# define KEY_LEFT			65361
+# define KEY_RIGHT			65363
+# define KEY_DOWN			65364
+
+# define KEY_Q				113
+# define KEY_ESC			65307
 
 #endif
