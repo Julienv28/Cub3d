ba858217c677a5689937e738b751c89cb41a6bad
Oceane Pique <opique@f2r5s11.clusters.42paris.fr>
2025-07-01 13:36:17 +0200

    correction norme et comm + textures

--- srcs/RayCasting/cast.c
+++ srcs/RayCasting/cast.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cast.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juvitry <juvitry@student.42.fr>            +#+  +:+       +#+        */
+/*   By: opique <opique@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 14:06:25 by juvitry           #+#    #+#             */
-/*   Updated: 2025/07/01 09:52:12 by juvitry          ###   ########.fr       */
+/*   Updated: 2025/07/01 13:34:11 by opique           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,44 +90,3 @@ void	render_game(t_data *data)
 		data->win_ptr, data->screen.xpm_ptr, 0, 0);
 	draw_minimap(data);
 }
-
-/*
-void	render_game(t_data *data)
-{
-	float	playerangle;
-	float	rayangle;
-	t_rc	rc;
-	int		ray;
-
-	clear_screen(&data->screen, 0x000000);
-	ray = 0;
-	playerangle = data->map.play.angle;
-	while (ray < NUM_RAYS)
-	{
-		rayangle = playerangle - (data->map.play.fov / 2)
-			+ (data->map.play.fov / NUM_RAYS) * ray;
-		rayangle = normalize_angle(rayangle);
-		rc.distance = get_dist_from_player(&data->map, rayangle, &rc);
-		if (rc.distance == FLT_MAX)
-			rc.distance = 20.0f;
-		rc.distance *= cosf(rayangle - playerangle);
-		if (rc.distance < 0.0001f)
-			rc.distance = 0.0001f;
-		rc.dis_proj_plane = (WIN_LEN / 2) / tan(data->map.play.fov / 2);
-		rc.pr_hght = ((rc.dis_proj_plane * TILE_SIZE) / rc.distance) * 
-		(float)WIN_LEN / NUM_RAYS;
-		if (rc.pr_hght >= WIN_HEIGHT)
-			rc.pr_hght = WIN_HEIGHT - 1;
-		rc.top_pixel = (WIN_HEIGHT / 2) - (rc.pr_hght / 2);
-		rc.bttm_pixel = (WIN_HEIGHT / 2) + (rc.pr_hght / 2);
-		if (rc.top_pixel < 0)
-			rc.top_pixel = 0;
-		if (rc.bttm_pixel > WIN_HEIGHT)
-			rc.bttm_pixel = WIN_HEIGHT;
-		draw_column(data, &rc, ray);
-		ray++;
-	}
-    mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, 
-	data->screen.xpm_ptr, 0, 0);
-	draw_minimap(data);
-}*/
